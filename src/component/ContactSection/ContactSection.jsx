4281351f5e78917bb290799c0a9091372df55684
Muhammad-Sabbir-Tech <g.shanto18@gmail.com>
2024-05-17 19:34:22 +0600

    working with contact

--- src/component/ContactSection/ContactSection.jsx
+++ src/component/ContactSection/ContactSection.jsx
@@ -36,6 +36,13 @@ function ContactSection(props) {
 
                                             <Col md={12}>
                                                 <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                    {/* <Form.Label>Name</Form.Label> */}
+                                                    <Form.Control size='lg' type="text" placeholder="Enter subject" />
+                                                </Form.Group>
+                                            </Col>
+
+                                            <Col md={12}>
+                                                <Form.Group className="mb-3" controlId="formBasicEmail">
                                                     {/* <Form.Label>Message</Form.Label> */}
                                                     <Form.Control size='lg' as="textarea" rows={6} placeholder="Enter message" />
                                                 </Form.Group>
@@ -51,43 +58,48 @@ function ContactSection(props) {
                         </Card>
                     </Col>
                     <Col lg={5}>
-                        <Card>
+                        <Card className={style.contactContentCard}>
                             <Card.Body>
-                                <div className="px-MD-3 py-4">
-                                    <h4 className="text-muted fw-bold pb-3">GET IN TOUCH WITH ME</h4>
+                                <div className="px-md-3 py-4">
+                                    <h4 className="fw-bold pb-3">GET IN TOUCH WITH ME</h4>
                                     <hr className="pb-4 mb-0" />
                                     <div className="mb-4">
-                                        <IoMailOutline style={{ fontSize: "30px" }} className="text-muted me-3" />
-                                        <a target="_blank" className="text-muted text-decoration-none" href="mailto:g.shanto18@gmail.com" style={{ fontSize: "20px" }}>g.shanto18@gmail.com</a>
+                                        <IoMailOutline style={{ fontSize: "30px" }} className=" me-3" />
+                                        <a target="_blank" className="text-decoration-none" href="mailto:g.shanto18@gmail.com" style={{ fontSize: "20px" }}>g.shanto18@gmail.com</a>
                                     </div>
 
                                     <div className="mb-4">
-                                        <IoIosLink style={{ fontSize: "30px" }} className="text-muted me-3" />
+                                        <IoIosLink style={{ fontSize: "30px" }} className=" me-3" />
                                         <a target="_blank" href="//www.sabbirinfo.com" className="text-muted text-decoration-none" style={{ fontSize: "20px" }}>www.sabbirinfo.com</a>
                                     </div>
 
-                                    <div className="mb-4 d-flex">
-                                        <IoLocationSharp style={{ fontSize: "30px" }} className="text-muted me-3" />
-                                        <div>
-                                            <a className="text-muted text-decoration-none" style={{ fontSize: "20px" }}>Moddho Katia, Satkhira Sadar, Satkhira, Khulna, Bangladesh.</a>
-                                        </div>
-                                    </div>
-
                                     <div className="mb-4">
-                                        <IoCallSharp style={{ fontSize: "30px" }} className="text-muted me-3" />
+                                        <IoCallSharp style={{ fontSize: "30px" }} className=" me-3" />
                                         <a className="text-muted text-decoration-none" target="_blank" href="tel:8801521396944" style={{ fontSize: "20px" }}>8801521396944</a>
                                     </div>
 
                                     <div className="mb-4">
-                                        <FaWhatsapp style={{ fontSize: "30px" }} className="text-muted me-3" />
+                                        <FaWhatsapp style={{ fontSize: "30px" }} className=" me-3" />
                                         <a target="_blank" href="whatsapp://send?&phone=8801789096018" className="text-muted text-decoration-none" style={{ fontSize: "20px" }}>8801789096018</a>
                                     </div>
 
-                                    <h4 className="text-muted fw-bold mt-5 pb-3">SOCIAL MEDIA</h4>
-                                    <hr className="pb-4 mb-0" />
-                                    <div className="d-flex align-items-center justify-content-center">
-                                        <a target="_blank" href="https://www.facebook.com/g.shanto18" className={style.headerSocialLink}><FaFacebookF /></a>
-                                        <a target="_blank" href="https://www.linkedin.com/in/muhammad-sabbir-hossain/" className={style.headerSocialLink}><FaLinkedinIn /></a>
+                                    <div className="mb-4">
+                                        <FaFacebookF style={{ fontSize: "30px" }} className=" me-3" />
+                                        <a target="_blank" href="https://www.facebook.com/g.shanto18" className="text-muted text-decoration-none" style={{ fontSize: "20px" }}>www.facebook.com/g.shanto18</a>
+                                    </div>
+
+                                    <div className="mb-4 d-flex">
+                                        <FaLinkedinIn style={{ fontSize: "30px" }} className=" me-3" />
+                                        <div>
+                                            <a target="_blank" href="https://www.linkedin.com/in/muhammad-sabbir-hossain/" className="text-muted text-decoration-none" style={{ fontSize: "20px" }}>www.linkedin.com/in/muhammad-sabbir-hossain/</a>
+                                        </div>
+                                    </div>
+
+                                    <div className="mb-4 d-flex">
+                                        <IoLocationSharp style={{ fontSize: "30px" }} className=" me-3" />
+                                        <div>
+                                            <a className="text-muted text-decoration-none" style={{ fontSize: "20px" }}>Moddho Katia, Satkhira Sadar, Satkhira, Khulna, Bangladesh.</a>
+                                        </div>
                                     </div>
                                 </div>
                             </Card.Body>
