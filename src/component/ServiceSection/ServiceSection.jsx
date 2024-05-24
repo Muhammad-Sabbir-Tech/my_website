44c291bac78df09847cfafebed87270e41c8fcc9
khomin <khomin91@gmail.com>
2024-05-24 15:52:04 +0530

    banner-update

--- src/component/ServiceSection/ServiceSection.jsx
+++ src/component/ServiceSection/ServiceSection.jsx
@@ -5,52 +5,65 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 function ServiceSection(props) {
     return (
         <>
-            <Container className="sectionDefaultMargin">
-                <h1 className="sectionHeaderTitle text-center mb-4">Services</h1>
-                <Row>
-                    <Col md={4} className="mb-3">
-                        <Card className="h-100">
-                            <Card.Body>
-                                <div className="text-center pb-3 pt-4">
-                                    <img style={{ height: "120px" }} src='/images/designAndDevelopment.png' />
-                                </div>
-                                <div className="text-center pb-4">
-                                    <h4 className="fw-bold">Website design & development</h4>
-                                    <p className="px-0 px-md-5">Design or Develop Website as Per Your Requiremnts from Scrach or Existing Design Templets</p>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+            <section className="serviceSection">
+                <Container className="sectionDefaultMargin">
 
-                    <Col md={4} className="mb-3">
-                        <Card className="h-100">
-                            <Card.Body>
-                                <div className="text-center pb-3 pt-4">
-                                    <img style={{ height: "120px" }} src='/images/webApplication.png' />
-                                </div>
-                                <div className="text-center pb-4">
-                                    <h4 className="fw-bold">Web application development</h4>
-                                    <p className="px-0 px-md-5">Develop High Secured and Professional Web Application / Software as Per Your Requiremnts </p>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    <Row>
+                        <Col md="5" sm="12" className="solutionTitle">
+                            <h4>Solutions</h4>
+                            <h2>Custom Website Development Tailored to Your Requirements</h2>
+                        </Col>
+                        <Col md="7" sm="12">
+                            <Row>
+                                <Col md={6} className="mb-3">
+                                    <Card className="h-100">
+                                        <Card.Body>
+                                            <div className="text-center pb-3 pt-4">
+                                                <img style={{ height: "120px" }} src='/images/designAndDevelopment.png' />
+                                            </div>
+                                            <div className="text-center pb-4">
+                                                <h4 className="fw-bold">Website design & development</h4>
+                                                <p className="px-0 px-md-5">Design or Develop Website as Per Your Requiremnts from Scrach or Existing Design Templets</p>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={6} className="mb-3">
+                                    <Card className="h-100">
+                                        <Card.Body>
+                                            <div className="text-center pb-3 pt-4">
+                                                <img style={{ height: "120px" }} src='/images/webApplication.png' />
+                                            </div>
+                                            <div className="text-center pb-4">
+                                                <h4 className="fw-bold">Web application development</h4>
+                                                <p className="px-0 px-md-5">Develop High Secured and Professional Web Application / Software as Per Your Requiremnts </p>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={6} className="mb-3">
+                                    <Card className="h-100">
+                                        <Card.Body>
+                                            <div className="text-center pb-3 pt-4">
+                                                <img style={{ height: "120px" }} src='/images/bugFix.png' />
+                                            </div>
+                                            <div className="text-center pb-4">
+                                                <h4 className="fw-bold">Bug Fixes</h4>
+                                                <p className="px-0 px-md-5">Fix any Bugs Related to PHP , MySQL , Laravel , CSS , JS</p>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
 
-                    <Col md={4} className="mb-3">
-                        <Card className="h-100">
-                            <Card.Body>
-                                <div className="text-center pb-3 pt-4">
-                                    <img style={{ height: "120px" }} src='/images/bugFix.png' />
-                                </div>
-                                <div className="text-center pb-4">
-                                    <h4 className="fw-bold">Bug Fixes</h4>
-                                    <p className="px-0 px-md-5">Fix any Bugs Related to PHP , MySQL , Laravel , CSS , JS</p>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+
+
+
+
+                    </Row>
+                </Container>
+            </section>
         </>
     );
 }
